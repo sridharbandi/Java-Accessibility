a93740f645d54da706f03ef2387423553290dfc0
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-04-11 22:42:31 +0200

    Axe Issue nodes Model

---
+++ src/main/java/io/github/sridharbandi/modal/axe/AxeNode.java
@@ -0,0 +1,22 @@
+package io.github.sridharbandi.modal.axe;
+
+public class AxeNode {
+    private String failureSummary;
+    private String html;
+
+    public String getFailureSummary() {
+        return failureSummary;
+    }
+
+    public void setFailureSummary(String failureSummary) {
+        this.failureSummary = failureSummary;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+}
