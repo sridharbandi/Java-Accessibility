e339e29bddaf3c212725ab76efed48b7fcd37dd5
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-04-12 17:08:54 +0200

    Process AXE Issues

--- src/main/java/io/github/sridharbandi/AXERunner.java
+++ src/main/java/io/github/sridharbandi/AXERunner.java
@@ -21,17 +21,30 @@
  */
 package io.github.sridharbandi;
 
+import io.github.sridharbandi.modal.Issue;
+import io.github.sridharbandi.modal.Issues;
+import io.github.sridharbandi.modal.axe.AxeIssue;
+import io.github.sridharbandi.modal.axe.AxeIssues;
+import io.github.sridharbandi.modal.axe.AxeNode;
 import io.github.sridharbandi.report.Result;
+import io.github.sridharbandi.util.DateUtil;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AXERunner extends Result {
 
     private static Logger LOG = LoggerFactory.getLogger(AXERunner.class);
     private Map<String, Object> issueList;
+    private Map<String, List<AxeIssue>> processedIssues;
+    private AxeIssues issues;
 
     public AXERunner(WebDriver driver) {
         super(driver);
@@ -44,5 +57,38 @@ public class AXERunner extends Result {
     public void execute(String pageName) {
         LOG.info("Running Accessibility for {} page", pageName);
         issueList = executeAxe();
+        processedIssues = axeIssueList(issueList);
+        issues = getIssues(pageName);
+        System.out.println("Hello");
     }
+
+    private AxeIssues getIssues(String reportName) {
+        AxeIssues issues = new AxeIssues();
+        issues.setCritical(issueCount("critical"));
+        issues.setSerious(issueCount("serious"));
+        issues.setModerate(issueCount("moderate"));
+        issues.setMinor(issueCount("minor"));
+        //Todo Change the Accessibility Standar Logic
+        issues.setStandard(Accessibility.STANDARD.name());
+        issues.setUrl(url());
+        issues.setDate(DateUtil.getDate());
+        issues.setSize(viewPort());
+        issues.setDevice(device());
+        issues.setBrowser(browserName());
+        issues.setName(reportName.isEmpty() ? pageTitle() : reportName);
+        issues.setReportID(UUID.randomUUID().toString().replace("-", ""));
+        issues.setIssues(processedIssues);
+        return issues;
+    }
+
+    private int issueCount(String impact) {
+        return processedIssues.values()
+                .stream()
+                .flatMap(List::stream)
+                .filter(axeIssue -> axeIssue.getImpact().equalsIgnoreCase(impact))
+                .map(AxeIssue::getNodes)
+                .mapToInt(List::size)
+                .sum();
+    }
+
 }
