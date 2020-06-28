e7884d95b331ea61dfa513a64107682b52af1c56
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-06-28 21:56:00 +0200

    Clean up

--- src/main/java/io/github/sridharbandi/report/Result.java
+++ src/main/java/io/github/sridharbandi/report/Result.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019 Sridhar Bandi.
+ * Copyright (c) 2020 Sridhar Bandi.
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
